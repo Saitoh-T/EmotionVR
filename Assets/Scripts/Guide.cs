ecd646ac13eeb01e3668a673619fde2d228f1b10
Saitoh-T <72367709+Saitoh-T@users.noreply.github.com>
2022-08-27 20:53:11 +0900

    Guide
    NavMeshAgentを利用して道を示してくれる

--- Assets/Scripts/Guide.cs
+++ Assets/Scripts/Guide.cs
@@ -1,27 +1,51 @@
 using UnityEngine;
 using System.Collections;
 
-public class TanksLookTarget : MonoBehaviour
+public class Guide : MonoBehaviour
 {
 
 	[SerializeField]
-	Transform target;
+	Transform[] targets;
 
 	[SerializeField]
 	Transform cursor;
 
 	[SerializeField]
-	NavMeshAgent agent;
+	UnityEngine.AI.NavMeshAgent agent;
+	
+	[SerializeField]
+	GameObject player;
+	
+	private InputActionMap _state;
 
 	void Start()
 	{
-		agent.SetDestination(target.position);
+		_state = player.GetComponent<InputActionMap>();
+		agent.SetDestination(targets[0].position);
 		agent.updateRotation = false;
 		agent.updatePosition = false;
 	}
 
 	void Update()
 	{
+		_state = player.GetComponent<InputActionMap>();
+
+		if (_state.item1 && _state.item2 && _state.item3)
+		{
+			agent.SetDestination(targets[3].position);
+		}
+		if (_state.item1 && _state.item2 && !_state.item3)
+		{
+			agent.SetDestination(targets[2].position);
+		}
+		if (_state.item1 && !_state.item2)
+		{
+			agent.SetDestination(targets[1].position);
+		}
+		if (!_state.item1)
+		{
+			agent.SetDestination(targets[0].position);
+		}
 		cursor.rotation = Quaternion.LookRotation(agent.steeringTarget - transform.position, Vector3.right);
 		agent.nextPosition = transform.position;
 	}
